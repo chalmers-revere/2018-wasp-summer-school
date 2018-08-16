fe7c3ac61316d09cf399962c66683910cffaae24
Christian Berger <christian.berger@gu.se>
2018-08-16 15:52:52 +0200

    * Example for steering, acceleration, deceleration
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- templates/image-postprocessing-opencv-python/myApplication.py
+++ templates/image-postprocessing-opencv-python/myApplication.py
@@ -16,34 +16,38 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-# sysv_ipc is needed to access the shared memory
+# sysv_ipc is needed to access the shared memory where the camera image is present.
 import sysv_ipc
-# numpy and cv2 are needed to access and display the pixels
+# numpy and cv2 are needed to access, modify, or display the pixels
 import numpy
 import cv2
 # OD4Session is needed to send and receive messages
 import OD4Session
-# Load the OpenDLV Standard Message Set.
+# Import the OpenDLV Standard Message Set.
 import opendlv_standard_message_set_v0_9_6_pb2
 
+################################################################################
 # This callback is triggered whenever there is a new distance reading coming in.
 def onDistance(msg, senderStamp, timeStamps):
-    print "Got distance; senderStamp=" + str(senderStamp)
+    print "Received distance; senderStamp=" + str(senderStamp)
     print "sent: " + str(timeStamps[0]) + ", received: " + str(timeStamps[1]) + ", sample time stamps: " + str(timeStamps[2])
     print msg
 
-# Create a session to send and receive messages from a running OD4Session at
-# CID 253 (replay mode).
-# TODO: Change to CID 112 when on the live system.
+# Create a session to send and receive messages from a running OD4Session;
+# Replay mode: CID = 253
+# Live mode: CID = 112
+# TODO: Change to CID 112 when this program is used on Kiwi.
 session = OD4Session.OD4Session(cid=253)
-# Register a receiver to handle a message; the following example is listening
+# Register a handler for a message; the following example is listening
 # for messageID 1039 which represents opendlv.proxy.DistanceReading.
+# Cf. here: https://github.com/chalmers-revere/opendlv.standard-message-set/blob/master/opendlv.odvd#L113-L115
 messageIDDistanceReading = 1039
 session.registerMessageCallback(messageIDDistanceReading, onDistance, opendlv_standard_message_set_v0_9_6_pb2.opendlv_proxy_DistanceReading)
-# Connect to the session.
+# Connect to the network session.
 session.connect()
 
-
+################################################################################
+# The following lines connect to the camera frame that resides in shared memory.
 # This name must match with the name used in the h264-decoder-viewer.yml file.
 name = "/tmp/img.argb"
 # Obtain the keys for the shared memory and semaphores.
@@ -55,12 +59,12 @@ shm = sysv_ipc.SharedMemory(keySharedMemory)
 mutex = sysv_ipc.Semaphore(keySemCondition)
 cond = sysv_ipc.Semaphore(keySemCondition)
 
-
-# Main loop to wait for the next image frame coming in.
+################################################################################
+# Main loop to process the next image frame coming in.
 while True:
     # Wait for next notification.
     cond.Z()
-    print "Got notified about new frame."
+    print "Received new frame."
 
     # Lock access to shared memory.
     mutex.acquire()
@@ -73,9 +77,10 @@ while True:
     # Unlock access to shared memory.
     mutex.release()
 
-    # Turn buf into img array (640 * 480 * 4 bytes (ARGB)).
+    # Turn buf into img array (640 * 480 * 4 bytes (ARGB)) to be used with OpenCV.
     img = numpy.frombuffer(buf, numpy.uint8).reshape(480, 640, 4)
 
+    ############################################################################
     # TODO: Add some image processing logic here.
 
     # The following example is adding a red rectangle and displaying the result.
@@ -85,9 +90,27 @@ while True:
     #cv2.imshow("image", img);
     #cv2.waitKey(2);
 
-    # Example for creating and sending a message to other microservices:
+    ############################################################################
+    # Example for creating and sending a message to other microservices; can
+    # be removed when not needed.
     angleReading = opendlv_standard_message_set_v0_9_6_pb2.opendlv_proxy_AngleReading()
     angleReading.angle = 123.45
 
     # 1038 is the message ID for opendlv.proxy.AngleReading
     session.send(1038, angleReading.SerializeToString());
+
+    ############################################################################
+    # Steering and acceleration/decelration.
+    #
+    # Uncomment the following lines to steer; range: +38deg (left) .. -38deg (right).
+    # Value groundSteeringRequest.groundSteering must be given in radians (DEG/180. * PI).
+    #groundSteeringRequest = opendlv_standard_message_set_v0_9_6_pb2.opendlv_proxy_GroundSteeringRequest()
+    #groundSteeringRequest.groundSteering = 0
+    #session.send(1090, groundSteeringRequest.SerializeToString());
+
+    # Uncomment the following lines to accelerate/decelerate; range: +0.25 (forward) .. -1.0 (backwards).
+    # Be careful!
+    #pedalPositionRequest = opendlv_standard_message_set_v0_9_6_pb2.opendlv_proxy_PedalPositionRequest()
+    #pedalPositionRequest.position = 0
+    #session.send(1086, pedalPositionRequest.SerializeToString());
+
